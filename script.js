66900052b15464fe6bcd7b55575b8d345f130bdc
Grace Lim <98506014+gracelimshiern@users.noreply.github.com>
2023-01-12 17:23:01 +0800

    updated achievements

---
+++ script.js
@@ -0,0 +1,26 @@
+var index = 0;
+var images = document.querySelectorAll('.carousel-inner img');
+
+function prev() {
+  images[index].style.display = 'none';
+  index--;
+  if (index < 0) {
+    index = images.length - 1;
+  }
+  images[index].style.display = 'block';
+}
+
+function next() {
+  images[index].style.display = 'none';
+  index++;
+  if (index >= images.length) {
+    index = 0;
+  }
+  images[index].style.display = 'block';
+}
+
+for (var i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+}
+
+images[0].style.display = 'block';
